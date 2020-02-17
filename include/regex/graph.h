e63e6feae20dcd34b21ed5403b66b25b03e1c17f
inhzus <inhzus@gmail.com>
2020-02-17 21:51:08 +0800

    [Feat] graph compilation

--- include/regex/graph.h
+++ include/regex/graph.h
@@ -4,44 +4,67 @@
 #ifndef REGEX_GRAPH_H_
 #define REGEX_GRAPH_H_
 
+#include <memory>
 #include <string>
 #include <utility>
 #include <vector>
 
 namespace regex {
 
-struct Edge {
-  enum Type { Char, Wildcard, Empty };
+struct Node;
 
-  explicit Edge(Type type) : ch(), type(type) {}
-  explicit Edge(char ch) : ch(ch), type(Char) {}
+struct Edge {
+  enum Type { Char, Any, Empty };
+
+  Edge(Type type, Node *node) : ch(), type(type), next(node) {}
+  Edge(char ch, Node *node) : ch(ch), type(Char), next(node) {}
 
   char ch;
   Type type;
+  Node *next;
 };
 
 struct Node {
   enum Status { Default, Match };
 
-  Node() : status(Default) {}
-  Node(Status status, std::vector<Node *> edges) :
-      status(status), edges(std::move(edges)) {}
+  Node() : ref(0), status(Default) {}
+  explicit Node(size_t ref) : ref(ref), status(Default) {}
+  explicit Node(std::vector<Edge> edges) :
+      ref(0), status(Default), edges(std::move(edges)) {}
+  Node(size_t ref, std::vector<Edge> edges) :
+      ref(ref), status(Default), edges(std::move(edges)) {}
+//  Node(Status status, std::vector<Edge> edges) :
+//      status(status), edges(std::move(edges)) {}
 
-  static Node MatchNode() {
-    return Node(Match, std::vector<Node *>());
-  }
+//  static Node MatchNode() {
+//    return Node(Match, std::vector<Edge>());
+//  }
 
+  size_t ref;
   Status status;
-  std::vector<Node *> edges;
+  std::vector<Edge> edges;
 };
 
 struct Segment {
   explicit Segment(Node *start) : start(start) {}
-
-  static Segment Parse(const std::string &s);
+  Segment(Node *start, Node *end) :
+      start(start), ends({end}) {}
 
   Node *start;
   std::vector<Node *> ends;
+};
+
+class Graph {
+ public:
+  static Graph Compile(const std::string &s);
+
+  explicit Graph(Segment seg) : seg_(std::move(seg)) {}
+  ~Graph();
+
+  bool Match(const std::string &s) const;
+
+ private:
+  Segment seg_;
 };
 
 }  // namespace regex
--- src/graph.cc
+++ src/graph.cc
@@ -4,4 +4,74 @@
 
 #include "regex/graph.h"
 
+#include <cassert>
+#include <queue>
+#include <stack>
+
 #include "regex/regex.h"
+
+namespace regex {
+
+struct Char {
+  static const char kAny = '*', kConcat = '.', kQuest = '?', kPlus = '+';
+};
+
+Graph Graph::Compile(const std::string &s) {
+  std::stack<Segment> stack;
+  for (char ch : s) {
+    switch (ch) {
+      case Char::kAny: {
+        break;
+      }
+      case Char::kConcat: {
+        Segment back(stack.top());
+        stack.pop();
+        Segment &front(stack.top());
+        std::for_each(
+            front.ends.begin(), front.ends.end(),
+            [&back = back](Node *node) {
+              node->edges.emplace_back(Edge::Empty, back.start);
+              ++back.start->ref;
+            });
+        front.ends = std::move(back.ends);
+        break;
+      }
+      case Char::kQuest: {
+        break;
+      }
+      case Char::kPlus: {
+        break;
+      }
+      default: {
+        auto node = new Node();
+        node->edges.emplace_back(ch, new Node(1));
+        Segment segment(node, node->edges.back().next);
+        stack.push(std::move(segment));
+        break;
+      }
+    }
+  }
+  assert(stack.size() == 1);
+  Segment &seg(stack.top());
+  assert(seg.ends.size() == 1);
+  seg.ends.front()->status = Node::Match;
+  seg.ends.clear();
+  return Graph(seg);
+}
+
+Graph::~Graph() {
+  std::queue<Node *> queue;
+  queue.push(seg_.start);
+  while (!queue.empty()) {
+    Node *node = queue.front();
+    queue.pop();
+    for (auto &edge : node->edges) {
+      edge.next->ref--;
+      queue.push(edge.next);
+    }
+    if (node->ref == 0)
+      delete (node);
+  }
+}
+
+}  // namespace regex
